d37e52f923106bfde41bc826ccae8d6d515bf318
Anderson Mendes Barros <pe22gug@eduvaud.ch>
2024-09-13 14:24:29 +0200

    feat

---
+++ src/weeklyDataSwitchCase.js
@@ -0,0 +1,22 @@
+function getDaySwitch(day) {
+    switch(day) {
+        case 1:
+            return "Lundi";
+        case 2:
+            return "Mardi";
+        case 3:
+            return "Mercredi";
+        case 4:
+            return "Jeudi";
+        case 5:
+            return "Vendredi";
+        case 6:
+            return "Samedi";
+        case 7:
+            return "Dimanche";
+        default:
+            return "Numéro de jour invalide";
+    }
+}
+
+console.log(getDaySwitch(3));  // "Mercredi"
